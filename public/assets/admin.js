295f148371cf9802ffdd19140a876f16504e7791
Errlangga <132829116+Errlangga@users.noreply.github.com>
2026-09-25 21:54:14 +0700

    cleanup: keep only authoritative Blob path after upload

--- public/assets/admin.js
+++ public/assets/admin.js
@@ -43,7 +43,7 @@ async function uploadBlob(file,kind,progressId){
     xhr.onabort=function(){reject(new Error('Upload dibatalkan.'));};
     xhr.send(file);
   });
-  return {mediaUrl:info.mediaUrl,mediaOrigin:info.mediaOrigin,mediaPath:info.mediaPath,mediaType:info.mediaType};
+  return {mediaPath:info.mediaPath,mediaType:info.mediaType};
 }
 
 async function refresh(){
